25f94620218339891184d7528d693ae1c03daf42
John Rojcewicz <rojcewiczj@gmail.com>
2019-09-10 13:36:30 -0500

    working on reducer

---
+++ my-app/src/reducers/reducer.js
@@ -0,0 +1,15 @@
+
+const state = [{
+    item: 'Learn about reducers',
+  completed: false,
+  id: 3892987589
+},
+{
+ item: 'practice keyboard',
+ completed: false,
+ id: 2534523452
+}];
+
+const reducer= (state,action) {
+    return
+}
