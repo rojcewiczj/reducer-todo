ce5dd3fef3227e2181cf9d9f117c54877efe87fc
John Rojcewicz <rojcewiczj@gmail.com>
2019-09-10 14:17:20 -0500

    working on newTask component

--- my-app/src/components/newTask.js
+++ my-app/src/components/newTask.js
@@ -1,1 +1,15 @@
 import React, { useState, useReducer } from 'react';
+import { initialState, todoReducer } from '../reducers/reducer';
+
+const newTask = () => {
+    const [newTaskText, setNewTaskText] = useState();
+    const [] = useReducer
+    const handleChanges = e => {
+        setNewTaskText({[e.target.name]: e.target.value})
+    }
+    return (
+        <div>
+
+        </div>
+    )
+}
