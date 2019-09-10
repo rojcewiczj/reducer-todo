dec7ecd7a512c26cbc85836289d94809152f6061
John Rojcewicz <rojcewiczj@gmail.com>
2019-09-10 14:11:41 -0500

    working on new task component

--- my-app/src/reducers/reducer.js
+++ my-app/src/reducers/reducer.js
@@ -1,15 +1,22 @@
+import React from 'react';
 
-const state = [{
-    item: 'Learn about reducers',
-  completed: false,
-  id: 3892987589
-},
-{
- item: 'practice keyboard',
- completed: false,
- id: 2534523452
-}];
+export const initialState = {
+    todoList: [ 
+         { item: 'Learn about reducers',
+           completed: false,
+           id: 3892987589
+             },
+        {
+          item: 'practice keyboard',
+          completed: false,
+          id: 2534523452
+            },
+        ]
+};
 
-const reducer= (state,action) {
-    return
+export const todoReducer = (state, action) => {
+   return state;
 }
+
+//toggle_completed === toggle editing,
+// new task === updat_title
